585a798eaddef4011949e30b030c12ecba408378
centa <centa@bogon.(none)>
2012-08-31 23:43:22 +0800

    Initial Commit

---
+++ Poj/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Poj
+//
+//  Created by centa on 12-8-31.
+//  Copyright (c) 2012年 centa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
